6fb42cd0faf07c628be4079f4586ce051ffdcea4
durlavk98 <durlavk98@gmail.com>
2021-07-31 23:53:33 +0530

    added tailwind and basics configured

---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="px-2 py-2 flex justify-between items-center">
+    <p class="text-blue-500 text-xl font-bold">odinbook</p>
+    <i class="fa fa-search bg-gray-100 p-1"></i>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+
+</style>
